7478657ac4cb0bfa25b6e6f28bb33206cc110826
jose <josejab95@gmail.com>
2023-03-11 04:52:01 +0100

    cors

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,12 +4,12 @@
 
 export const environment = {
   production: false,
-  url: "https://jabtwit-back.vercel.app/api"
+  url: "http://localhost:3000"
 };
 
 /*
  * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
  *
  * This import should be commented out in production mode because it will have a negative impact
  * on performance if an error is thrown.
