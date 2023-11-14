d2ac55126106a96b798664e9c7705e89725dba0a
jose <josejab95@gmail.com>
2023-11-14 14:30:25 +0100

    añade logs

--- src/app/services/tweets.service.ts
+++ src/app/services/tweets.service.ts
@@ -2,109 +2,107 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { Tweet } from '../models/Tweet';
-import { LoadingService } from './loading.service';
-import { map, finalize } from "rxjs"
-import { startsWith } from 'lodash';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TweetsService {
 
-  url: string = environment.url
+  private readonly _apiUrl: string = `${environment.url}/tweet`
 
-  constructor(private http: HttpClient, private _loadingService: LoadingService) { }
+  constructor(private http: HttpClient) { }
 
 
   public postTweet(tweet: Tweet) {
 
-    return this.http.post(`${this.url}/tweet/add`, tweet)
+    return this.http.post<Tweet>(`${this._apiUrl}/add`, tweet)
   }
 
   public postImagenEnTweet(imagen: Blob, filename: string) {
 
     const profileData = new FormData()
 
+    console.log("imagen: " + imagen)
+    console.log("imagen filename: " + filename)
     profileData.append("image", imagen, filename)
 
-    return this.http.post(`${this.url}/tweet/addImageTweet`, profileData)
+    return this.http.post<Tweet>(`${this._apiUrl}/addImageTweet`, profileData)
   }
 
-  public getTwets(contadorCargaTweets) {
+  public getTwets(contadorCargaTweets: number) {
 
     const params = new HttpParams()
       .set('page', contadorCargaTweets)
 
 
-    return this.http.get<Tweet>(`${this.url}/tweet/getTweets`, { params })
+    return this.http.get<Tweet>(`${this._apiUrl}/getTweets`, { params })
 
   }
 
-  public getTwetsByProfile(user, contadorCargaTweets) {
+  public getTwetsByProfile(user: string, contadorCargaTweets: number) {
 
     const params = new HttpParams()
       .set('page', contadorCargaTweets)
       .set('user', user)
 
 
-    return this.http.get(`${this.url}/tweet/getTweetsByProfile`, { params })
+    return this.http.get<Tweet>(`${this._apiUrl}/getTweetsByProfile`, { params })
   }
 
-  public getTweetsBeforeDate(contadorCargaTweets, fecha) {
+  public getTweetsBeforeDate(contadorCargaTweets: number, fecha: Date) {
 
     const params = new HttpParams()
       .set('page', contadorCargaTweets)
-      .set('fecha', fecha)
+      .set('fecha', fecha?.toString())
 
 
-    return this.http.get(`${this.url}/tweet/getTweetsBeforeDate`, { params })
+    return this.http.get<Tweet>(`${this._apiUrl}/getTweetsBeforeDate`, { params })
   }
 
-  public getTweetsBeforeDateByUser(contadorCargaTweets, fecha, user) {
+  public getTweetsBeforeDateByUser(contadorCargaTweets: number, fecha: Date, user: string) {
 
     const params = new HttpParams()
       .set('page', contadorCargaTweets)
-      .set('fecha', fecha)
+      .set('fecha', fecha?.toString())
       .set('user', user)
 
 
-    return this.http.get(`${this.url}/tweet/getTweetsBeforeDateByUser`, { params })
+    return this.http.get<Tweet>(`${this._apiUrl}/getTweetsBeforeDateByUser`, { params })
   }
 
 
-  public getTweetsAfterDate(contadorCargaTweets, fecha) {
+  public getTweetsAfterDate(contadorCargaTweets: number, fecha: Date) {
 
     const params = new HttpParams()
       .set('page', contadorCargaTweets)
-      .set('fecha', fecha)
+      .set('fecha', fecha?.toString())
 
-
-    return this.http.get(`${this.url}/tweet/getTweetsAfterDate`, { params })
+    return this.http.get<Tweet>(`${this._apiUrl}/getTweetsAfterDate`, { params })
   }
 
-  public getTweetsAfterDateByUser(contadorCargaTweets, fecha, user) {
+  public getTweetsAfterDateByUser(contadorCargaTweets: number, fecha: Date, user: string) {
 
     const params = new HttpParams()
       .set('page', contadorCargaTweets)
-      .set('fecha', fecha)
+      .set('fecha', fecha?.toString())
       .set('user', user)
 
 
-    return this.http.get(`${this.url}/tweet/getTweetsAfterDateByUser`, { params })
+    return this.http.get<Tweet>(`${this._apiUrl}/getTweetsAfterDateByUser`, { params })
   }
 
 
   public getCountTweets() {
 
 
-    return this.http.get(`${this.url}/tweet/getCountTweets`)
+    return this.http.get<number>(`${this._apiUrl}/getCountTweets`)
   }
 
 
   public deleteTweet(id: string) {
 
 
-    return this.http.delete(`${this.url}/tweet/remove/${id}`)
+    return this.http.delete(`${this._apiUrl}/remove/${id}`)
   }
 
 
